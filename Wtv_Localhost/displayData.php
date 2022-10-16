3a516eed65cc276ac395f5b43fb0c6d8dbeb9afd
kolarv <100720389+kolarv@users.noreply.github.com>
2022-10-16 13:03:30 +0200

    Add files via upload

--- Wtv_Localhost/displayData.php
+++ Wtv_Localhost/displayData.php
@@ -1,26 +1,20 @@
 <?php
-$conn=mysqli_connect('localhost','root','','testbase');
-    $query = "SELECT * FROM `users`;";
-    $result = $conn->query($query);
+   $mysqli = new mysqli('localhost','root','','testbase');
+   $array = array();
+   if ($mysqli -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    exit();
+  }
+  if ($result = $mysqli -> query("SELECT * FROM users")) {
     
-    
-    if ($result->num_rows > 0) 
-    {
-        while($row = $result->fetch_assoc())
-        {
-            echo
-                $row["ID"]. "  Name: " .
-                $row["Name"]. "  Email: " . 
-                $row["Email"]."<br>";
-        }
-         echo '<button type="button" onclick="alertuj()">Click Me!</button> <br> <script>
-        function alertuj(){
-        alert("test");
-        } </script>';
-    } 
-    else {
-        echo "0 results";
-    }
-  
-   $conn->close();
+    $array = $result;
+  }
+  foreach($array as $val){
+    echo $val['ID'];
+    echo " ";
+    echo $val['Name'];
+    echo " ";
+    echo $val['Email'];
+    echo "<br>";
+  }
 ?>
